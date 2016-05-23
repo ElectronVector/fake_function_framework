c4ae254b43b8dd2f4f74f5d12b81998ba72b16da
mchernosky <matt@chernosky.net>
2016-05-22 22:30:45 -0600

    Reverted to use a local copy of Ceedling for the integration tests.
    This reverts commit 2fcfe723443dfa4aaea6ec224762878df64133fe.

--- examples/fff_example/rakefile.rb
+++ examples/fff_example/rakefile.rb
@@ -1,4 +1,7 @@
-# Use the installed Ceedling gem for this example.
-require 'ceedling'
+# This change from the default is for running Ceedling out of another folder.
+PROJECT_CEEDLING_ROOT = "../../../.."
+load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
 
 Ceedling.load_project
+
+task :default => %w[ test:all release ]
